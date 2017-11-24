0c7daebe1d6f84cb613d2d708ae4b82a8a0c96d6
juan2ramos <juan2ramos@gmail.com>
2017-11-23 20:05:10 -0500

    Remove duplicated directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path                    = require('path');
 const webpack                 = require('webpack');
 const ExtractTextPlugin       = require("extract-text-webpack-plugin");
-const UglifyJsPlugin          = require('uglifyjs-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const BrowserSyncPlugin       = require('browser-sync-webpack-plugin');
 const GoogleFontsPlugin       = require("google-fonts-webpack-plugin");
 
 const config = {
@@ -12,48 +10,25 @@ const config = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].js'
+        filename: 'js/inuit.js'
     },
     module: {
         rules: [
             {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'postcss-loader', 'sass-loader']
+                    fallback: 'style-loader?sourceMap',
+                    use: ['css-loader?sourceMap', 'postcss-loader', 'sass-loader'],
+
                 })
-            },
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
             }
         ]
     },
     plugins: [
-        new ExtractTextPlugin('/inuit.css'),
-        new BrowserSyncPlugin({
-            proxy: 'inuitcss.dev',
-            port: 3000,
-            files: [
-                '**/*.php'
-            ],
-            ghostMode: {
-                clicks: false,
-                location: false,
-                forms: false,
-                scroll: false
-            },
-            injectChanges: true,
-            logFileChanges: true,
-            logLevel: 'debug',
-            logPrefix: 'inuit',
-            notify: true,
-            reloadDelay: 0
+        new ExtractTextPlugin({
+            filename: '/inuit.css?sourceMap'
         }),
+
         new GoogleFontsPlugin({
             fonts: [
                 { family: "Source Sans Pro" },
@@ -61,23 +36,18 @@ const config = {
             ],
             local: false,
         })
-    ]
+    ],
+    devtool: 'source-map'
 };
 
 //If true JS and CSS files will be
 if (process.env.NODE_ENV === 'production') {
     config.plugins.push(
-        new UglifyJsPlugin({
-            sourceMap: true
-        }),
-        new OptimizeCssAssetsPlugin(),
-        new webpack.SourceMapDevToolPlugin({
-            append: "\n//# sourceMappingURL=http://example.com/sourcemap/[url]",
-            filename: '[name].js.map',
-        })
 
-
+        new ExtractTextPlugin('/inuit.min.css'),
+        new OptimizeCssAssetsPlugin()
     );
+
 }
 
 module.exports = config;
